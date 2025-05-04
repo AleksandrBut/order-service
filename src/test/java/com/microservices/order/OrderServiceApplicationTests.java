7e076c70b3ed0ee82a66bbc1c322dabb0215364d
AleksandrBut <alex638000@gmail.com>
2025-05-04 18:01:28 +0300

    test(order): implement integration test

--- src/test/java/com/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,51 @@
 package com.microservices.order;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldCreateOrder() {
+        String requestBody = """
+                {
+                    "skuCode": "iph_1",
+                    "price" : 123.456,
+                    "quantity": 25
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(JSON)
+                .body(requestBody)
+                .when()
+                .post("api/order")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("orderNumber", notNullValue())
+                .body("skuCode", equalTo("iph_1"))
+                .body("price", equalTo(123.456F))
+                .body("quantity", equalTo(25));
+    }
 }
